35050d12e2d1b984c57fbf52601c1648742245f3
Jacob Livingston <jacob.m.livingston@gmail.com>
2018-08-10 13:41:48 -0600

    still working on bullet collisions

--- js/bullet.js
+++ js/bullet.js
@@ -3,6 +3,7 @@ class Bullet{
     this.pos=pos;
     this.vel=vel;
     this.r=10
+    this.gravity=true
   }
   draw(){
     canvas.front.ctx.fillStyle="rgb(94, 74, 107)"
@@ -14,7 +15,10 @@ class Bullet{
   move(){
     this.pos.x+=this.vel.x;
     this.pos.y+=this.vel.y
-    this.vel.y+=.1
+    if(this.gravity){
+      this.vel.y+=.1
+
+    }
   }
   collide(){
     let x     = Math.floor(this.pos.x);
@@ -32,15 +36,17 @@ class Bullet{
           left--;
         }
         let angle = Math.atan2(game.ground.points[left].y-game.ground.points[right].y,game.ground.points[left].x-game.ground.points[right].x)
-        let adjAgnle=angle-Math.PI/2
+        let adjAgnle=angle-Math.PI
 
         let aBtP=Math.atan2(this.pos.y-game.ground.points[x].y,this.pos.x-game.ground.points[x].x)-Math.PI
-        let xPos=Math.cos(aBtP)*dist+this.pos.x-this.r
-        let yPos=Math.sin(aBtP)*dist+this.pos.y-this.r
+        let xPos=Math.cos(aBtP)*dist+this.pos.x+this.r
+        let yPos=Math.sin(aBtP)*dist+this.pos.y+this.r
         this.pos.x=xPos;
         this.pos.y=yPos;
-        this.vel.x=Math.cos(adjAgnle)*this.vel.x
-        this.vel.y=Math.sin(adjAgnle)*this.vel.y
+
+        this.vel.x=Math.cos(adjAgnle)*this.vel.getMagnitude()
+        this.vel.y=Math.sin(adjAgnle)*this.vel.getMagnitude()
+        // this.gravity=false
         // console.log("here");
 
       }
